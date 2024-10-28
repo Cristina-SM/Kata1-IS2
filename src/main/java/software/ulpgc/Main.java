8a0fdd09766c0e480af4a04f04113acaac901303
Cristina-SM <cristinasantanamartindevelop@gmail.com>
2024-10-28 13:46:56 +0000

    create Main with Person class

---
+++ src/main/java/software/ulpgc/Main.java
@@ -0,0 +1,9 @@
+package software.ulpgc;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Person("Cristina", LocalDate.of(2001,10, 1)));
+    }
+}
